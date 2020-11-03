bdc91b1a0bb69139ccdcd1c713df1096aa16d42b
Gaji <gajarajdharmaraj@gmail.com>
2020-11-03 21:28:37 +0530

    my Java test app

---
+++ app/src/main/java/com/example/javatest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.javatest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b=(Button)findViewById(R.id.button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(i);
+
+            }
+        });
+    }
+}
